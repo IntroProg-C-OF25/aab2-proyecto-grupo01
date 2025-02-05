bef8ccdb26eba6a330a2c3923704a69d2d40a764
PatitoCorrea <rafaelcorrealopez21@gmail.com>
2025-02-05 12:00:39 -0500

    Proyecto Bimestral 2

--- SOLUCION/ProyectoBIM2/src/ProyectoBim2/entradas/entradas.java
+++ SOLUCION/ProyectoBIM2/src/ProyectoBim2/entradas/entradas.java
@@ -4,46 +4,33 @@ import java.util.Scanner;
 
 public class entradas {
    
-    public static int comprarEntradaNormal(Scanner tcl) {
+    public static int[] comprarEntradaNormal(Scanner tcl) {
         int precio = 5;
-        int precioT = 0;
-        int entradas;
-        System.out.print("¿Cuantas entradas desea comprar?🤔: ");
-        entradas = tcl.nextInt();
-        for (int i = 0; i < entradas; i++) {
-            precioT += precio;
-        }
-        System.out.println("✅ Entradas normales comprada por $" + precioT);
-        return precioT;
+        System.out.print("¿Cuántas entradas desea comprar?🤔: ");
+        int entradas = tcl.nextInt();
+        int precioT = entradas * precio;
+        System.out.println("✅ Entradas normales compradas por $" + precioT);
+        return new int[]{precioT, entradas};
     }
 
-    public static int comprarEntradaEspecial(Scanner tcl) {
+    public static int[] comprarEntradaEspecial(Scanner tcl) {
         int precioBase = 10;
-        int precioT = 0;
-        int entradas;
+        System.out.print("¿Cuántas entradas desea comprar?🤔: ");
+        int entradas = tcl.nextInt();
         String dia;
-        System.out.print("¿Cuantas entradas desea comprar?🤔");
-        entradas=tcl.nextInt();
-        do{
+        do {
             System.out.print("Ingrese el día de la semana (jueves, viernes, sábado): ");
             dia = tcl.next().toLowerCase();
-            
-     
             if (dia.equals("jueves")) {
                 precioBase = 8;
             } else if (dia.equals("viernes") || dia.equals("sábado")) {
                 precioBase = 12;
+            } else {
+                System.err.println("Error! solo puede ingresar el día: jueves, viernes y sábado");
             }
-   
-            if (dia.equals("lunes") || dia.equals("martes") || dia.equals("miercoles") || dia.equals("domingo")){
-                System.err.println("Error! solo puede ingresar el dia: jueves, viernes y sabado");
-            }
-        }while (dia.equals("lunes") || dia.equals("martes") || dia.equals("miercoles") || dia.equals("domingo") );
-        for (int i = 0; i < entradas; i++) {
-                precioT += precioBase;
-        }
-
-        System.out.println("🎟️ Entradas especiales compradas por $" + precioT);
-        return precioT;
+        } while (!dia.equals("jueves") && !dia.equals("viernes") && !dia.equals("sábado"));
+        int precioT = entradas * precioBase;
+        System.out.println("🎟 Entradas especiales compradas por $" + precioT);
+        return new int[]{precioT, entradas};
     }
 }
